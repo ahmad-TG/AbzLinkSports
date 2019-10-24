73d8deae9a0bbfec12e93352309b4eb06871f592
ahmad-TG <ahmadroyani913@gmail.com>
2019-10-24 15:31:41 +0700

    abz

---
+++ abzlinksport_spring/src/main/java/com/abzlinksport/abzlinksport_online/AbzlinksportOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.abzlinksport.abzlinksport_online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbzlinksportOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbzlinksportOnlineApplication.class, args);
+	}
+
+}
---
+++ abzlinksport_spring/src/test/java/com/abzlinksport/abzlinksport_online/AbzlinksportOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abzlinksport.abzlinksport_online;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbzlinksportOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
